fd17e61c65b4f6d02460ecdde754180efb3d3b27
Karabo Molema <komolema@gmail.com>
2024-09-12 08:24:04 +0200

    more tests

--- src/test/kotlin/org/example/orderbook/MatchingEngineImplTest.kt
+++ src/test/kotlin/org/example/orderbook/MatchingEngineImplTest.kt
@@ -26,8 +26,34 @@ class MatchingEngineImplTest {
     }
 
     @Test
+    fun `fill order when buy price is higher to sell price and quantities are the same`() {
+        val buyOrder = Order(
+            side = OrderSide.BUY, quantity = 1000.00, price = 1.5, currencyPair = CurrencyPair.BTCZAR)
+        val sellOrder = Order(side = OrderSide.SELL, quantity = 1000.00, price = 1.0, currencyPair = CurrencyPair.BTCZAR)
+
+        val orderMatchResult = matchingEngine.limitMatchOrder(buyOrder, sellOrder)
+
+        assertTrue(orderMatchResult is OrderMatchEvent.OrderFilled)
+        assertEquals(buyOrder.id, (orderMatchResult as OrderMatchEvent.OrderFilled).buyOrder.id)
+        assertEquals(sellOrder.id, (orderMatchResult as OrderMatchEvent.OrderFilled).sellOrder.id)
+    }
+
+    @Test
     fun `partial fill when buy order quantity is less than sell order quantity`() {
         val buyOrder = Order(side = OrderSide.BUY, quantity = 500.00, price = 1.0, currencyPair = CurrencyPair.BTCZAR)
+        val sellOrder = Order(side = OrderSide.SELL, quantity = 1000.00, price = 1.0, currencyPair = CurrencyPair.BTCZAR)
+
+        val orderMatchResult = matchingEngine.limitMatchOrder(buyOrder, sellOrder)
+
+        assertTrue(orderMatchResult is OrderMatchEvent.PartialOrderFilled)
+        assertNull((orderMatchResult as OrderMatchEvent.PartialOrderFilled).partialBuyOrder.orNull())
+        assertNotNull((orderMatchResult).partialSellOrder.orNull())
+        assertEquals(500.00, (orderMatchResult).partialSellOrder.orNull()?.quantity)
+    }
+
+    @Test
+    fun `partial fill when buy order quantity is less than sell order quantity and price is higher`() {
+        val buyOrder = Order(side = OrderSide.BUY, quantity = 500.00, price = 3.0, currencyPair = CurrencyPair.BTCZAR)
         val sellOrder = Order(side = OrderSide.SELL, quantity = 1000.00, price = 1.0, currencyPair = CurrencyPair.BTCZAR)
 
         val orderMatchResult = matchingEngine.limitMatchOrder(buyOrder, sellOrder)
@@ -60,4 +86,7 @@ class MatchingEngineImplTest {
 
         assertTrue(orderMatchResult is OrderMatchEvent.OrderNotFilled)
     }
+
+    
+    
 }
